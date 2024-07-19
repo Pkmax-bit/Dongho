2fffc900f93d588183691f9a6efea83eb267ba20
Pkmax-bit <2100010779@nttu.edu.vn>
2024-07-19 14:11:10 +0700

    1

---
+++ dongho 2/UserControl1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace dongho_2
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnstart = new System.Windows.Forms.Button();
+            this.btnlap = new System.Windows.Forms.Button();
+            this.lbltime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnstart
+            // 
+            this.btnstart.Location = new System.Drawing.Point(241, 267);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(75, 23);
+            this.btnstart.TabIndex = 0;
+            this.btnstart.Text = "Start";
+            this.btnstart.UseVisualStyleBackColor = true;
+            this.btnstart.Click += new System.EventHandler(this.btnstart_Click);
+            // 
+            // btnlap
+            // 
+            this.btnlap.Location = new System.Drawing.Point(469, 267);
+            this.btnlap.Name = "btnlap";
+            this.btnlap.Size = new System.Drawing.Size(75, 23);
+            this.btnlap.TabIndex = 1;
+            this.btnlap.Text = "Lap";
+            this.btnlap.UseVisualStyleBackColor = true;
+            this.btnlap.Click += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbltime
+            // 
+            this.lbltime.AutoSize = true;
+            this.lbltime.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltime.Location = new System.Drawing.Point(238, 153);
+            this.lbltime.Name = "lbltime";
+            this.lbltime.Size = new System.Drawing.Size(306, 55);
+            this.lbltime.TabIndex = 2;
+            this.lbltime.Text = "00:00:00 000";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lbltime);
+            this.Controls.Add(this.btnlap);
+            this.Controls.Add(this.btnstart);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(800, 450);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnstart;
+        private System.Windows.Forms.Button btnlap;
+        private System.Windows.Forms.Label lbltime;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ dongho 2/UserControl1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dongho_2
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+        const string START_TEXT = "Start";
+        const string STOP_TEXT = "Stop";
+        DateTime timeStart;
+        private void btnstart_Click(object sender, EventArgs e)
+        {
+            if (btnstart.Text == START_TEXT)
+            {
+                btnstart.Text = STOP_TEXT;
+                timer1.Enabled = true;
+                timeStart = DateTime.Now;
+            }
+            else
+            {
+                btnstart.Text = START_TEXT;
+                timer1.Enabled = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lbltime.Text = (DateTime.Now - timeStart).ToString();
+        }
+    }
+}
